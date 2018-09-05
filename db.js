c998c8e7964293ec7be855fa83b42100bcd03314
Syed Faraaz Ahmad <faraaz98@live.com>
2018-09-05 19:59:05 +0530

    refactor to separate db code

--- db.js
+++ db.js
@@ -1,17 +1,35 @@
-let { Pool } = require("pg");
+let { Pool } = require('pg');
 
-let pool = new Pool({
+const pool = new Pool({
     user: 'faraaz',
+    host: 'localhost',
     database: 'chat',
-    password: "skffs7461010",
+    password: process.env.PASSWORD,
     port: 5432,
 });
 
-pool.query('select * from messages', (err, res) => {
-    if (err) {
-        console.error(err);
-    }
-    else {
-        console.log(res);
-    }
-});
+function insertQuery(msg) {
+    let body = msg.body;
+    let sender = msg.sender;
+    let timestamp = msg.created_at;
+
+    [body, sender, timestamp].forEach((child) => {
+        if (typeof(child) != String) {
+            return Error(`${child} must be string`);
+        }
+    });
+
+    let queryString = `insert into messages \
+    (body, sender, created_at) \
+    values ('${body}', '${sender}', '${timestamp}'); \
+    `;
+
+    return queryString;
+}
+
+const db = {
+    pool: pool, 
+    insertQuery: insertQuery
+};
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-let { Pool } = require('pg');
+let express = require('express');
+let port = process.env.PORT || 3000;
 
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+const db = require('./db');
+
+// folder for static files
 app.use('/public', express.static(__dirname + '/public'));
-
-const pool = new Pool({
-  user: 'faraaz',
-  host: 'localhost',
-  database: 'chat',
-  password: process.env.PASSWORD,
-  port: 5432,
-});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -22,7 +16,7 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg.body);
-    pool.query(`insert into messages (body, sender, created_at) values ('${msg.body}', '${msg.sender}', '${msg.created_at}');`, (err, res) => {
+    db.pool.query(db.insertQuery(msg), (err, res) => {
       if (err) {
         console.error(err);
       }
