5b5fe643f07f01b148d748019b3abd021eab763d
Syed Faraaz Ahmad <faraaz98@live.com>
2018-09-13 23:51:48 +0530

    add dialect to db config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,14 @@
 module.exports = {
   'development': {
-    database_url: process.env.DATABASE_URL
+    database_url: process.env.DATABASE_URL,
+    dialect: 'postgres'
   },
   'test': {
-    database_url: process.env.DATABASE_URL
+    database_url: process.env.DATABASE_URL,
+    dialect: 'postgres'
   },
   'production': {
-    database_url: process.env.DATABASE_URL
+    database_url: process.env.DATABASE_URL,
+    dialect: 'postgres'
   }
 }
